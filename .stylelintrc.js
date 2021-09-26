e5e393bc44e8e1b5efafbc16f025b44a5e0c62c5
huliang <hu_liang56@163.com>
2021-09-26 18:10:07 +0800

    add stylelint order

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,4 +2,9 @@ const fabric = require('@umijs/fabric');
 
 module.exports = {
   ...fabric.stylelint,
+  extends: ['stylelint-config-rational-order'],
+  plugins: ['stylelint-order', 'stylelint-declaration-block-no-ignored-properties'],
+  rules: {
+    'plugin/declaration-block-no-ignored-properties': true,
+  },
 };
